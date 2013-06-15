d445bfdc4b696e65a60ce64ed7b1d24c02109f97
Hugo Alvarez Alcazar <aahugo92@gmail.com>
2013-06-14 22:17:54 -0600

    Acceso y Logica de Socios y .bak base
    Se creó la lógica y el acceso de los socios y se sube el .bak de la base
    con los socios añadidos.

---
+++ SistemaContable/SistemaContable/BaseDatos/AccesoSocios.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BibliotecasComunes;
+using System.Data.SqlClient;
+
+namespace SistemaContable.BaseDatos
+{
+    public static class AccesoSocios
+    {
+        public static List<Socio> obtenerSocios(object[] pArgumentos)
+        {
+            List<Socio> resultado = new List<Socio>();
+
+            SqlDataReader dataReader = null;
+            try
+            {
+                dataReader = SqlServer.Instance.traerDataReader("obtenerSocios", pArgumentos);
+                if (dataReader.HasRows)
+                {
+                    while (dataReader.Read())
+                    {
+                        Socio socio = new Socio();
+                        socio.Codigo = dataReader[0].ToString();
+                        socio.Nombre = dataReader[1].ToString();
+                        socio.LimiteLocal = Double.Parse(dataReader[2].ToString());
+                        socio.CondicionPago = Int32.Parse(dataReader[3].ToString());
+                        socio.TipoSocio = dataReader[4].ToString();
+                        
+                        Cuenta cuenta = new Cuenta();
+                        cuenta.CodigoCuenta = dataReader[5].ToString();
+                        cuenta.Nombre = dataReader[6].ToString();
+                        cuenta.SaldoLocal = Double.Parse(dataReader[7].ToString());
+
+                        socio.Cuenta = cuenta;
+
+                        Moneda moneda = new Moneda();
+                        moneda.Nombre = dataReader[8].ToString();
+                        moneda.Codigo = dataReader[9].ToString();
+
+                        socio.Moneda = moneda;
+
+                        resultado.Add(socio);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                SqlServer.Instance.CerrarConexion();
+            }
+            return resultado;
+        }
+
+        public static void insertarSocio(object[] pArgumentos)
+        {
+            try
+            {
+                SqlServer.Instance.ejecutar("insertarSocio", pArgumentos);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                SqlServer.Instance.CerrarConexion();
+            }
+        }
+
+        public static List<string> obtenerTiposSocios()
+        {
+            List<string> resultado = new List<string>();
+            SqlDataReader dataReader = null;
+            try
+            {
+                dataReader = SqlServer.Instance.traerDataReader("obtenerTiposSocios");
+                if (dataReader.HasRows)
+                {
+                    while (dataReader.Read())
+                    {
+                        resultado.Add(dataReader[0].ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                SqlServer.Instance.CerrarConexion();
+            }
+            return resultado;
+        }
+    }
+}
---
+++ SistemaContable/SistemaContable/Logica/LogicaSocios.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BibliotecasComunes;
+using SistemaContable.BaseDatos;
+
+namespace SistemaContable.Logica
+{
+    public static class LogicaSocios
+    {
+        public static List<Socio> obtenerSocios(string pEmpresa)
+        {
+            try
+            {
+                return AccesoSocios.obtenerSocios(new object[] { pEmpresa });
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public static List<string> obtenerTiposSocios()
+        {
+            try
+            {
+                return AccesoSocios.obtenerTiposSocios();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public static void insertarSocio(string pEmpresa, string pCodigoMoneda, int pidCuentaMayor, string pTipoSocio, string pCodigoSocio, string pNombreSocio, double pLimiteLocal, int pCondicion)
+        {
+            try
+            {
+                AccesoSocios.insertarSocio(new object[] { pEmpresa, pCodigoMoneda, pidCuentaMayor, pTipoSocio, pCodigoSocio, pNombreSocio, pLimiteLocal, pCondicion });
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
