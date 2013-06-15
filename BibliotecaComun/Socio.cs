eebb1a7de75dbcdedeab280d3ad67768d467cf75
Hugo Alvarez Alcazar <aahugo92@gmail.com>
2013-06-14 20:22:48 -0600

    Creé la clase Socio
    Hice una clase socio en las Bibliotecas Comunes

---
+++ BibliotecaComun/Socio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BibliotecasComunes
+{
+    public class Socio
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public double LimiteLocal { get; set; }
+        public int CondicionPago { get; set; }
+        public string TipoSocio { get; set; }
+        public Cuenta Cuenta { get; set; }
+        public Moneda Moneda { get; set; }
+    }
+}
